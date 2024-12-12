b968ca62d8a0e71c259fdaf1f9a6ddd5c8b803d7
Nguyen Hai Duong <haiduongnguyen0209@gmail.com>
2024-12-13 00:27:17 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useRef, useState } from "react";
 import { IRefPhaserGame, PhaserGame } from "./game/PhaserGame";
-import { MainMenu } from "./game/scenes/MainMenu";
+import MainMenu from "./game/scenes/MainMenu";
 
 function App() {
     //  References to the PhaserGame component (game and scene are exposed)
