8db22cc4fecdf4c45dacfb2ed621a8cfcdfae2a9
Nguyen Hai Duong <haiduongnguyen0209@gmail.com>
2025-01-07 21:18:36 +0700

    delete preloader

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -1,44 +1,48 @@
 import Phaser, { Time } from "phaser";
-import { EventBus } from '../EventBus';
+import { EventBus } from "../EventBus";
 
 export default class MainMenu extends Phaser.Scene {
+    constructor() {
+        super("MainMenu");
+    }
 
-	constructor() {
-		super("MainMenu");
-	}
-
-	private logo!: Phaser.GameObjects.Image;
+    private logo!: Phaser.GameObjects.Image;
 
     logoTween: Phaser.Tweens.Tween | null;
 
     startTime: number;
     outsideMessage: Phaser.GameObjects.Text;
 
-    create ()
-    {
+    create() {
         this.add.image(512, 384, "background");
 
-		// logo
-		const logo = this.add.image(512, 384, "logo");
+        // logo
+        const logo = this.add.image(512, 384, "logo");
 
-		// text
-		const text = this.add.text(512, 460, "", {});
-		text.setOrigin(0.5, 0.5);
-		text.text = "Main Menu";
-		text.setStyle({ "align": "center", "color": "#ffffff", "fontFamily": "Arial Black", "fontSize": "38px", "stroke": "#000000", "strokeThickness":8});
+        // text
+        const text = this.add.text(512, 460, "", {});
+        text.setOrigin(0.5, 0.5);
+        text.text = "Main Menu";
+        text.setStyle({
+            align: "center",
+            color: "#ffffff",
+            fontFamily: "Arial Black",
+            fontSize: "38px",
+            stroke: "#000000",
+            strokeThickness: 8,
+        });
 
-		this.logo = logo;
+        this.logo = logo;
 
-		this.events.emit("scene-awake");
+        this.events.emit("scene-awake");
 
         // Communication
         this.startTime = this.game.getTime();
-        this.outsideMessage = this.add.text(0, 0, '');
-        EventBus.emit('current-scene-ready', this);
+        this.outsideMessage = this.add.text(0, 0, "");
+        EventBus.emit("current-scene-ready", this);
     }
 
-    outsideIn (message: string, callback: (message: string) => void)
-    {
+    outsideIn(message: string, callback: (message: string) => void) {
         // Communication: Show message from outside
         this.outsideMessage.setText(message);
 
@@ -46,9 +50,8 @@ export default class MainMenu extends Phaser.Scene {
         this.insideOut(callback);
     }
 
-    insideOut (callback: (message: string) => void)
-    {
-       callback(Math.floor(this.game.getTime() - this.startTime) + '');
+    insideOut(callback: (message: string) => void) {
+        callback(Math.floor(this.game.getTime() - this.startTime) + "");
     }
 }
 
